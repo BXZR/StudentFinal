3a0f045caa6230793f80fe2dccb52bd188505ee0
BXZR <405181821@qq.com>
2018-11-20 18:08:48 +0800

    开始界面UI
    开始界面UI
    学生时代最后一个作品了 ，果然还是放不下仙剑三
    至于仙剑四的开始界面，我决定作为一个通关之后二周目的开始界面
    这是永远的回忆，也是我目标的根源所在

---
+++ Assets/Codes/UI/StartCanvas/YinYangMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YinYangMove : MonoBehaviour {
+
+	//阴阳图的移动
+	public float moveDistance = 2f;
+
+	//两个坐标，分别是起点和终点
+	private Vector3 startPosition;
+	private Vector3 endPosition;
+	private Vector3 aimNow;//当前移动目标
+	private bool isMoving = false;
+
+
+	void Start () 
+	{
+		startPosition = this.transform.position;
+		endPosition = this.transform.position + new Vector3 (moveDistance , 0f , 0f);
+		aimNow = startPosition;
+
+		StartMove ();
+	}
+
+
+	public void StartMove()
+	{
+		isMoving = true;
+		aimNow = aimNow == startPosition ? endPosition : startPosition;
+		CancelInvoke ();
+		Invoke ("endMove" , 5f);
+	}
+
+	private void  endMove()
+	{
+		isMoving = false;
+	}
+
+	void Update()
+	{ 
+		if(isMoving)
+		this.transform.position = Vector3.Lerp (this.transform.position ,  aimNow , 0.2f);
+	}
+
+}
