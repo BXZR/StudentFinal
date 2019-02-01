9f3aa0c9ceba2271b119eb079e2731ef2dfd2312
BXZR <405181821@qq.com>
2019-02-01 17:26:36 +0800

    声音控制相关脚本
    BGM和音效的控制脚本加入，还没有生效

---
+++ Assets/Codes/Audio/BGMController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent (typeof( AudioSource))]
+public class BGMController : MonoBehaviour {
+
+
+	public static BGMController theBGMController = null;
+	List<AudioClip> audioClips = new List<AudioClip>();
+	//这个类是用于BGM切换的
+	public AudioSource sourceBGM;
+
+	void Start () {
+		theBGMController = this;
+		sourceBGM = this.GetComponent<AudioSource>();
+		sourceBGM.loop = true;
+		ChangeBGM("costumNormal");
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	public void ChangeBGM(string BGMName)
+	{
+		if (!sourceBGM)
+			return;
+
+		for (int i = 0; i < audioClips.Count; i++)
+		{
+			if (audioClips[i].name == BGMName)
+			{
+				sourceBGM.clip = audioClips[i];
+				sourceBGM.Play();
+				return;
+			}
+		}
+
+		try
+		{
+			AudioClip theClip = Resources.Load<AudioClip>("Music/" + BGMName);
+			if (theClip == null)
+				return;
+
+			theClip.name = BGMName;
+			audioClips.Add(theClip);
+			sourceBGM.clip = theClip;
+			sourceBGM.Play();
+		}
+		catch
+		{
+			return;
+		}
+	}
+}
---
+++ Assets/Codes/Audio/SoundController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	[RequireComponent(typeof(AudioSource))]
+	public class SoundPlayer : MonoBehaviour
+	{
+
+		public static SoundPlayer soundPlayer = null;
+		private List<AudioClip> clips = new List<AudioClip>();
+		AudioClip theClip;
+		//这个类是用于BGM切换的
+		private AudioSource Source;
+
+		void Start()
+		{
+			soundPlayer = this;
+			Source = GetComponent<AudioSource>();
+			Source.loop = false;
+			// DontDestroyOnLoad(this.gameObject);
+		}
+
+		public void StopSound()
+		{
+			if (Source.isPlaying)
+				Source.Stop();
+		}
+
+
+
+		public void PlaySound(string SoundName)
+		{
+			if (!Source)
+				return;
+
+			if (theClip != null && theClip.name == SoundName && Source.isPlaying)
+				return;
+
+			for (int i = 0; i < clips.Count; i++)
+			{
+				if (clips[i].name == SoundName)
+				{
+					Source.PlayOneShot(clips[i]);
+					return;
+				}
+
+			}
+			theClip = Resources.Load<AudioClip>("Audio/" + SoundName);
+			if (theClip == null)
+				return;
+			theClip.name = SoundName;
+			Source.PlayOneShot(theClip);
+			clips.Add(theClip);
+		}
+
+	}
